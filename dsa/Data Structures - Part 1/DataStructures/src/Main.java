b1fc68abe3b0bfe95f990cfdd153dcf99459bbb7
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2023-01-02 15:30:31 -0800

    reverse a string using a stack on main

--- dsa/Data Structures - Part 1/DataStructures/src/Main.java
+++ dsa/Data Structures - Part 1/DataStructures/src/Main.java
@@ -1,23 +1,35 @@
 import com.codewithmosh.MyLinkedList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] names) {
-        MyLinkedList<Integer> list = new MyLinkedList<>();
-        list.addFirst(20);
-        list.addFirst(30);
-        list.addLast(40);
-        //list.addLast(50);
-        System.out.println(list.size());
-        //list.deleteFirst();
-        //list.deleteFirst();
-        //list.deleteLast();
-        System.out.println(list.contains(20));
-        System.out.println(list.indexOf(30));
-        System.out.println(Arrays.toString(list.toArray()));
-        //list.reverse();
-        System.out.println(Arrays.toString(list.toArray()));
-        System.out.println(list.getKthFromTheEnd(2));
-        list.printMiddle();
+        //MyLinkedList<Integer> list = new MyLinkedList<>();
+        //list.addFirst(20);
+        //list.addFirst(30);
+        //list.addLast(40);
+        ////list.addLast(50);
+        //System.out.println(list.size());
+        ////list.deleteFirst();
+        ////list.deleteFirst();
+        ////list.deleteLast();
+        //System.out.println(list.contains(20));
+        //System.out.println(list.indexOf(30));
+        //System.out.println(Arrays.toString(list.toArray()));
+        ////list.reverse();
+        //System.out.println(Arrays.toString(list.toArray()));
+        //System.out.println(list.getKthFromTheEnd(2));
+        //list.printMiddle();
+
+        Stack<Character> stack = new Stack<>();
+        String input = "julian";
+        StringBuffer inputReversed = new StringBuffer();
+
+        for(char ch : input.toCharArray())
+            stack.push(ch);
+
+        while(!stack.empty())
+            inputReversed.append(stack.pop());
+
+        System.out.println(inputReversed);
     }
 }
